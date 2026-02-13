a5c735a67ff23a7c7415c199b4b21f4d5015feb9
Fireshoa <29lindsayr@apcsd.org>
2026-02-13 20:38:22 +0000

    Added new testing mod :)

---
+++ mods/test.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!")
+
+elements.black_matter = {
+    color: "#000000",
+    behavior: [
+        "XX|DL%5|XX",
+        "DL%5|XX|DL%5",
+        "M2|M1 AND DL%5|M2"
+    ],
+    category: "special"
+}
