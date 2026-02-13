a48003abeff608760215677daf6ea49723362477
Fireshoa <29lindsayr@apcsd.org>
2026-02-13 20:46:13 +0000

    Updated test modified

--- mods/test.js
+++ mods/test.js
@@ -3,9 +3,26 @@ console.log("Hello, World!")
 elements.black_matter = {
     color: "#000000",
     behavior: [
-        "XX|DL%5|XX",
-        "DL%5|XX|DL%5",
-        "M2|M1 AND DL%5|M2"
+        "XX|M1 AND DL|XX",
+        "M1 AND DL|XX|M1 AND DL",
+        "XX|M1 AND DL|XX"
     ],
-    category: "special"
+    category: "special",
+    temp: 5000,
+    tempLow: 90,
+    state: "gas",
+    stateLow: "white_matter"
+}
+
+elements.white_matter = {
+    color: "#000000",
+    behavior: [
+        "XX|DL|XX",
+        "DL|XX|DL",
+        "M2|M1 AND DL|M2"
+    ],
+    category: "special",
+    temp: 5000,
+    tempHigh: 110,
+    stateHigh: "black_matter"
 }
